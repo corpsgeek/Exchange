00fb935aba3baaa5d88bcd4087b3b8163c58b124
corpsgeek <teamcyclone8@gmail.com>
2018-06-27 15:55:57 +0100

    chhg

--- sw.js
+++ sw.js
@@ -10,21 +10,27 @@ let cacheFiles = [
 
 self.addEventListener('install', function(e){
     console.log("[service worker], Installed");
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache){
+            console.log("[service worker] caching files");
+            return cache.addAll(cacheFiles);
+        })
+    )
    
 })
 
 self.addEventListener('activate', function(e){
     console.log("[service worker], activated");
-    e.waitUntil(
-        caches.open(cacheName).then(function(cache){
-            console.log("[service worker] caching files");
-            return cache.addAll('./',
-            'index.html',
-            './css/main.css',
-            '/app.js',
-            );
-        })
-    )
+  e.waitUntil(
+      caches.keys().then(function(cacheNames){
+          return promise.all(cacheNames.map(function(thisCacheName){
+              if(thisCacheName !== cacheName){
+                  console.log("Service worker removing cache file");
+                  return caches.delete(thisCacheName);
+              }
+          }))
+      })
+  )
 })
 
 self.addEventListener('fetch', function(e){
