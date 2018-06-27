13ce90658719c9b711968058b173cf69a1c5a4a5
corpsgeek <teamcyclone8@gmail.com>
2018-06-27 15:46:36 +0100

    Adding cache

--- sw.js
+++ sw.js
@@ -1,5 +1,21 @@
+let cacheName = 'v1';
+let cacheFiles = [
+    './',
+    'index.html',
+    './css/main.css',
+    '/app.js',
+    
+]
+
+
 self.addEventListener('install', function(e){
     console.log("[service worker], Installed");
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache){
+            console.log("[service worker] caching files");
+            return cache.addAll(cacheFiles);
+        })
+    )
 })
 
 self.addEventListener('activate', function(e){
