aab1afbd86429ff00ecdac3cf0fc3a68920791c7
corpsgeek <teamcyclone8@gmail.com>
2018-06-27 18:52:48 +0100

    ff

--- idb.js
+++ idb.js
@@ -1,10 +1,30 @@
-
+	
+var idbSupported = false;
+var db;
+ 
 document.addEventListener("DOMContentLoaded", function(){
-  
-     if("indexedDB" in window) {
-         console.log("YES!!! I CAN DO IT!!! WOOT!!!");
-     } else {
-         console.log("I has a sad.");
-     }
-  
- },false);
+ 
+    if("indexedDB" in window) {
+        idbSupported = true;
+    }
+ 
+    if(idbSupported) {
+        var openRequest = indexedDB.open("test",1);
+ 
+        openRequest.onupgradeneeded = function(e) {
+            console.log("Upgrading...");
+        }
+ 
+        openRequest.onsuccess = function(e) {
+            console.log("Success!");
+            db = e.target.result;
+        }
+ 
+        openRequest.onerror = function(e) {
+            console.log("Error");
+            console.dir(e);
+        }
+ 
+    }
+ 
+},false);
