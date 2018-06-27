1fb7f9678c5287129885579358c7f0890ba6cbf1
corpsgeek <teamcyclone8@gmail.com>
2018-06-27 18:29:47 +0100

    ff

--- idb-test/idb.js
+++ idb-test/idb.js
@@ -1,5 +1,6 @@
 import idb from'idb';
 
 idb.open('Currencydb', 1, function(upgradeDb){
-    
+  var keyValStore = upgradeDb.createObjectStore('keyval');
+  keyValStore.put('hello', 'greeting');  
 })
