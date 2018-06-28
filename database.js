00f68ffae379b946c5d582723fa2d4e521b5ef5f
corpsgeek <teamcyclone8@gmail.com>
2018-06-28 21:40:28 +0100

    dd

--- database.js
+++ database.js
@@ -16,7 +16,7 @@ const dbPromise = idb.open('Currency-converter-db', 1, upgradeDB => {
         const currSym = `${currency[key].currencySymbol}`;
 
       const tx = db.transaction('currencyName-and-Symbol', 'readwrite');
-      tx.objectStore('currencyName-and-Symbol').put(currency[key]);
+      tx.objectStore('currencyName-and-Symbol').put(currency[key].currencyName);
       return tx.complete;
     }
 
