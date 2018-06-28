1de5c2beb77b361e5263f94c36c51fdc01629468
corpsgeek <teamcyclone8@gmail.com>
2018-06-28 21:51:30 +0100

    jj

--- database.js
+++ database.js
@@ -9,15 +9,14 @@ const dbPromise = idb.open('Currency-converter-db', 1, upgradeDB => {
       return response.json();
     })
     .then(function(myJson) {
-        let key;
         const currency = myJson.results;
-    for( key in currency.currencyName){
-     
-      const tx = db.transaction('currencyName-and-Symbol', 'readwrite');
-      tx.objectStore('currencyName-and-Symbol').put(currency.currencyName[key]);
-      return tx.complete;
-    }
-
+    
+    for (i = 0; i < currency.length; i++) { 
+        const tx = db.transaction('currencyName-and-Symbol', 'readwrite');
+        tx.objectStore('currencyName-and-Symbol').put(currency[i]);
+        return tx.complete;
+        
+    } 
    
 });
 });
