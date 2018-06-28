8f2316fd95d4bff42cf53eb746732d4d0f2c8b24
corpsgeek <teamcyclone8@gmail.com>
2018-06-28 22:16:35 +0100

    ccjj

--- database.js
+++ database.js
@@ -1,19 +1,6 @@
 const dbPromise = idb.open('Currency-converter-db', 1, upgradeDB => {
-    switch (upgradeDB.oldVersion) {
-      case 0:
-        upgradeDB.createObjectStore('currencyName-and-Symbol');
-    }
+    var RatesStore = upgradeDB.createObjectStore('currencyRates');
   }).then(db => {
-    fetch('https://free.currencyconverterapi.com/api/v5/currencies')
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(myJson) {
-        const currency = myJson.results;
-    Object.values(currency).forEach(function(curr){
-        const tx = db.transaction('currencyName-and-Symbol', 'readwrite');
-        tx.objectStore('currencyName-and-Symbol').add(curr);
-    });
-   
-});
+  
+  
 });
