53bb2f7a36575611b35e10e60b766f87c0b7faba
corpsgeek <teamcyclone8@gmail.com>
2018-06-28 22:59:27 +0100

    ggggsg

--- database.js
+++ database.js
@@ -2,7 +2,7 @@
 
 var dbPromise =	idb.open('rates', 1, function(upgradeDB) {
 	  var ratesStore = upgradeDB.createObjectStore('rates', {
-		keyPath: 'id'});
+		keyPath: 'id', autoIncrement: true});
 		fetch('https://free.currencyconverterapi.com/api/v5/currencies')
 		.then(function(response) {
 		  return response.json();
@@ -10,8 +10,9 @@ var dbPromise =	idb.open('rates', 1, function(upgradeDB) {
 		.then(function(myJson) {
 		  const currency = myJson.results;
 		  for(let key in currency){
+              console.log(currency[key]);
             var tx = db.transaction('rates', 'readwrite');
-            tx.objectStore('rates').put(`${currency[key].id}`);
+            tx.objectStore('rates').add(currency[key]);
 
 		  }
 		  
