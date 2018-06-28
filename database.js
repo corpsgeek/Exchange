19e5b6b63c1da20cd4b9ebebae86bdaee9b3a9dc
corpsgeek <teamcyclone8@gmail.com>
2018-06-28 22:35:19 +0100

    ddd

--- database.js
+++
@@ -1,6 +0,0 @@
-const dbPromise = idb.open('Currency-converter-db', 1, upgradeDB => {
-    var RatesStore = upgradeDB.createObjectStore('currencyRates');
-  }).then(db => {
-  
-  
-});
--- sw.js
+++ sw.js
@@ -105,3 +105,23 @@ self.addEventListener('fetch', function(e) {
 			}) // end caches.match(e.request)
 	); // end e.respondWith
 });
+
+function createDB() {
+	idb.open('rates', 1, function(upgradeDB) {
+	  var ratesStore = upgradeDB.createObjectStore('rates', {
+		keyPath: 'id'});
+		fetch('https://free.currencyconverterapi.com/api/v5/currencies')
+		.then(function(response) {
+		  return response.json();
+		})
+		.then(function(myJson) {
+		  const currency = myJson.results;
+		  for(let key in currency){
+			  
+			  ratesStore.put({id: `${currency[key].id}`, name: 'coke', price: 10.99, quantity: 200});
+			  
+		  }
+		  
+		});
+	});
+  }
