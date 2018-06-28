03630f15acf1695dcbfffe76c31f3f6e5a1f94b1
corpsgeek <teamcyclone8@gmail.com>
2018-06-28 14:02:40 +0100

    push push

--- idb.js
+++ idb.js
@@ -11,3 +11,41 @@ if (!window.indexedDB) {
 }else{
   console.log("IndexedDb is up and running");
 }
+
+const dbName = "Currencies_Name";
+
+var request = indexedDB.open(dbName, 1);
+
+request.onerror = function(event) {
+  // Handle errors.
+  console.log("oops!, Database failed to create");
+};
+request.onupgradeneeded = function(event) {
+  var db = event.target.result;
+
+  // Create an objectStore to hold information about our customers. We're
+  // going to use "ssn" as our key path because it's guaranteed to be
+  // unique - or at least that's what I was told during the kickoff meeting.
+  var currencyNameStore = db.createObjectStore("Currencies Name", { keyPath: "Currency Name" });
+  var currenciesNameFetched;
+
+  objectStore.transaction.oncomplete = function(event) {
+    // Store values in the newly created objectStore.
+    var  currencyNameStore = db.transaction("Curencies Name", "readwrite");
+    fetch('https://free.currencyconverterapi.com/api/v5/currencies')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      const currency = myJson.results;
+      for(let key in currency){
+        currenciesNameFetched = `${currency[key].currencyName}`;             
+      }
+         
+    
+    });
+    currenciesNameFetched.forEach(function(currencyNames) {
+      currencyNameStore.add(currencyNames);
+    });
+  };
+};
