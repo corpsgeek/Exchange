03c32133b8985bed91bf7f14b65faa5d762343fa
corpsgeek <teamcyclone8@gmail.com>
2018-06-28 19:44:22 +0100

    dd

--- app.js
+++ app.js
@@ -77,3 +77,27 @@ if ('serviceWorker' in navigator) {
         })
     
 }
+
+
+
+
+
+
+const dbPromise = idb.open('keyval-store', 2, upgradeDB => {
+  // Note: we don't use 'break' in this switch statement,
+  // the fall-through behaviour is what we want.
+  switch (upgradeDB.oldVersion) {
+    case 0:
+      upgradeDB.createObjectStore('keyval');
+    case 1:
+      upgradeDB.createObjectStore('objs', {keyPath: 'id'});
+  }
+});
+dbPromise.then(db => {
+  const tx = db.transaction('objs', 'readwrite');
+  tx.objectStore('objs').put({
+    id: 123456,
+    data: {foo: "bar"}
+  });
+  return tx.complete;
+});
