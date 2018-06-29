4150b1c980bdf0f20c3abc460589b9ad170964c2
corpsgeek <teamcyclone8@gmail.com>
2018-06-29 13:19:51 +0100

    Completed project, final commit.

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
-
-var cacheName = 'v1';
+const cacheName = 'v1';
 // Default files to always cache
-var cacheFiles = [
+const cacheFiles = [
 	'/Exchange/',
 	'/Exchange/index.html',
     '/Exchange/app.js',
@@ -12,17 +11,17 @@ var cacheFiles = [
 	'https://png.icons8.com/small/64/000000/shekel.png',
     
 	
-]
+];
 
 
-self.addEventListener('install', function(e) {
+self.addEventListener('install', e => {
     console.log('[ServiceWorker] Installed');
 
     // e.waitUntil Delays the event until the Promise is resolved
     e.waitUntil(
 
     	// Open the cache
-	    caches.open(cacheName).then(function(cache) {
+	    caches.open(cacheName).then(cache => {
 
 	    	// Add all the default files to the cache
 			console.log('[ServiceWorker] Caching cacheFiles');
@@ -32,30 +31,28 @@ self.addEventListener('install', function(e) {
 });
 
 
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', e => {
     console.log('[ServiceWorker] Activated');
 	  
     e.waitUntil(
 		
     	// Get all the cache keys (cacheName)
-		caches.keys().then(function(cacheNames) {
-			return Promise.all(cacheNames.map(function(thisCacheName) {
+		caches.keys().then(cacheNames => Promise.all(cacheNames.map(thisCacheName => {
 
-				// If a cached item is saved under a previous cacheName
-				if (thisCacheName !== cacheName) {
+            // If a cached item is saved under a previous cacheName
+            if (thisCacheName !== cacheName) {
 
-					// Delete that cached file
-					console.log('[ServiceWorker] Removing Cached Files from Cache - ', thisCacheName);
-					return caches.delete(thisCacheName);
-				}
-			}));
-		})
+                // Delete that cached file
+                console.log('[ServiceWorker] Removing Cached Files from Cache - ', thisCacheName);
+                return caches.delete(thisCacheName);
+            }
+        })))
 	); // end e.waitUntil
 
 });
 
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', e => {
 	console.log('[ServiceWorker] Fetch', e.request.url);
 
 	// e.respondWidth Responds to the fetch event
@@ -65,7 +62,7 @@ self.addEventListener('fetch', function(e) {
 		caches.match(e.request)
 
 
-			.then(function(response) {
+			.then(response => {
 
 				// If the request is in the cache
 				if ( response ) {
@@ -76,19 +73,19 @@ self.addEventListener('fetch', function(e) {
 
 				// If the request is NOT in the cache, fetch and cache
 
-				var requestClone = e.request.clone();
+				const requestClone = e.request.clone();
 				return fetch(requestClone)
-					.then(function(response) {
+					.then(response => {
 
 						if ( !response ) {
 							console.log("[ServiceWorker] No response from fetch ")
 							return response;
 						}
 
-						var responseClone = response.clone();
+						const responseClone = response.clone();
 
 						//  Open the cache
-						caches.open(cacheName).then(function(cache) {
+						caches.open(cacheName).then(cache => {
 
 							// Put the fetched response in the cache
 							cache.put(e.request, responseClone);
@@ -100,7 +97,7 @@ self.addEventListener('fetch', function(e) {
 				        }); // end caches.open
 
 					})
-					.catch(function(err) {
+					.catch(err => {
 						console.log('[ServiceWorker] Error Fetching & Caching New Data', err);
 					});
 
