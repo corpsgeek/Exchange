95f8d565205715b7346f09698043885bab26409c
corpsgeek <teamcyclone8@gmail.com>
2018-06-29 15:37:50 +0100

    reversed changes

--- app.js
+++ app.js
@@ -21,56 +21,7 @@ window.addEventListener('load', () => {
      //looping through the currency and psuhing item to the option tag of select lists.
      for(let key in currency){
          document.getElementById("lists1").innerHTML += (`<option value = "${currency[key].id}">(${currency[key].currencySymbol})${currency[key].id} - ${currency[key].currencyName}</option>`)
-         function sortListDir() {
-          var list, i, switching, b, shouldSwitch, dir, switchcount = 0;
-          list = document.getElementById("id01");
-          switching = true;
-          // Set the sorting direction to ascending:
-          dir = "asc"; 
-          // Make a loop that will continue until no switching has been done:
-          while (switching) {
-            // Start by saying: no switching is done:
-            switching = false;
-            b = list.getElementsByTagName("LI");
-            // Loop through all list-items:
-            for (i = 0; i < (b.length - 1); i++) {
-              // Start by saying there should be no switching:
-              shouldSwitch = false;
-              /* Check if the next item should switch place with the current item,
-              based on the sorting direction (asc or desc): */
-              if (dir == "asc") {
-                if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
-                  /* If next item is alphabetically lower than current item,
-                  mark as a switch and break the loop: */
-                  shouldSwitch = true;
-                  break;
-                }
-              } else if (dir == "desc") {
-                if (b[i].innerHTML.toLowerCase() < b[i + 1].innerHTML.toLowerCase()) {
-                  /* If next item is alphabetically higher than current item,
-                  mark as a switch and break the loop: */
-                  shouldSwitch= true;
-                  break;
-                }
-              }
-            }
-            if (shouldSwitch) {
-              /* If a switch has been marked, make the switch
-              and mark that a switch has been done: */
-              b[i].parentNode.insertBefore(b[i + 1], b[i]);
-              switching = true;
-              // Each time a switch is done, increase switchcount by 1:
-              switchcount ++;
-            } else {
-              /* If no switching has been done AND the direction is "asc",
-              set the direction to "desc" and run the while loop again. */
-              if (switchcount == 0 && dir == "asc") {
-                dir = "desc";
-                switching = true;
-              }
-            }
-          }
-        }
+        
      }
      for(let list in currency){
          document.getElementById("lists2").innerHTML += (`<option value = "${currency[list].id}">(${currency[list].currencySymbol})${currency[list].id} - ${currency[list].currencyName}</option>`)
